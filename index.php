b004c45ac7203f8f1854306fb44052385e806e95
berkay <berkaysurmeli@hotmail.com>
2018-07-07 13:24:12 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Amerigear</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="header">
+		<h1>AMERIGEAR</h1>
+		<nav>
+			<li><a href="#">Nasil Calisir?</a></li>
+			<li><a href="#">Gumruk vergileri</a></li>
+			<li><a href="#">Hakkimizda</a></li>
+		</nav>
+	</div><!--header-->
+	<div class="content">
+		
+	</div><!--content-->
+	<div class="footer">
+		<p><?php echo "Copyright GetNeoned LLC " . date("Y"); ?>
+	</div><!--footer-->
+
+</body>
+</html>
